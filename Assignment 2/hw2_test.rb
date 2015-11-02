671dfee0f81b5738a5e44b3ec0cce8f6a18a9bd1
Ryan Bernstein <ryan.bernstein@pdx.edu>
2015-11-01 18:27:45 -0800

    Fix indentation on hw2 tests. Add private file.

--- Assignment 2/hw2_test.rb
+++ Assignment 2/hw2_test.rb
@@ -3,23 +3,24 @@ require 'pry'
 require 'timeout'
 
 require_relative '../test_framework'
+require_relative 'private_tests'
 
 hw2_test = ->(directory) {
   Dir.chdir(directory) do
     begin
       @binary = attempt_compile
 
-    # For the public tests, we'll use the examples from the assignment spec
-    run(%w(4 4 a8), /12\.0/)
-    run(%w(4 4 1af), /-15\.5/)
-    run(%w(4 4 af), /15\.5/)
-    run(%w(3 3 3c), /[Nn]a[Nn]/)
-    run(%w(3 3 38), /[^-][Ii]nf/)
-    run(%w(3 3 78), /-[Ii]nf/)
-    run(%w(3 3 26), /3\.5/)
-    run(%w(3 3 18), /1\.0/)
-    run(%w(3 3 3f), /[Nn]a[Nn]/)
-    run(%w(3 3 37), /15\.0/)
+      # For the public tests, we'll use the examples from the assignment spec
+      run(%w(4 4 a8), /12\.0/)
+      run(%w(4 4 1af), /-15\.5/)
+      run(%w(4 4 af), /15\.5/)
+      run(%w(3 3 3c), /[Nn]a[Nn]/)
+      run(%w(3 3 38), /[^-][Ii]nf/)
+      run(%w(3 3 78), /-[Ii]nf/)
+      run(%w(3 3 26), /3\.5/)
+      run(%w(3 3 18), /1\.0/)
+      run(%w(3 3 3f), /[Nn]a[Nn]/)
+      run(%w(3 3 37), /15\.0/)
     rescue => e
       `rm #{@binary}` unless @binary.nil?
       raise e
